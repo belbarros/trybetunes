6376972b647731d9991d8e6e578191b3bb67a980
Izabel de Barros <izabelfeigel@gmail.com>
2022-07-05 16:52:45 -0300

    Req 8-11

--- src/components/MusicCard.js
+++ src/components/MusicCard.js
@@ -1,34 +1,83 @@
 import React from 'react';
 import propTypes from 'prop-types';
+import { addSong, removeSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+      isFav: false,
+    };
+  }
+
+  componentDidMount() {
+    this.isFavorite();
+  }
+
+  isFavorite = () => {
+    const { favorites, trackId } = this.props;
+    if (favorites.some((song) => song.trackId === trackId)) {
+      this.setState({ isFav: true });
+    } else {
+      this.setState({ isFav: false });
+    }
+  }
+
+  handleCheck = () => {
+    this.setState((prevState) => ({
+      loading: true,
+      isFav: !prevState.isFav,
+    }), async () => {
+      const { songList } = this.props;
+      const { isFav } = this.state;
+      if (isFav) {
+        await addSong(songList);
+        this.setState({ loading: false });
+      } else {
+        await removeSong(songList);
+        this.setState({ loading: false });
+      }
+    });
+  }
+
   render() {
-    const { songList } = this.props;
+    const { trackName, previewUrl, trackId } = this.props;
+    const { loading, isFav } = this.state;
+    if (loading) return <Loading />;
     return (
       <div>
-        {
-          songList.filter((a) => (a.kind === 'song'))
-            .map((b) => (
-              <div key={ b.trackName }>
-                <p>{ b.trackName }</p>
-                <audio data-testid="audio-component" src={ b.previewUrl } controls>
-                  <track kind="captions" />
-                  O seu navegador não suporta o elemento
-                  {' '}
-                  {' '}
-                  <code>audio</code>
-                  .
-                </audio>
-              </div>
-            ))
-        }
+        <p>{ trackName }</p>
+        <audio data-testid="audio-component" src={ previewUrl } controls>
+          <track kind="captions" />
+          O seu navegador não suporta o elemento
+          {' '}
+          {' '}
+          <code>audio</code>
+          .
+        </audio>
+        <label htmlFor={ trackId }>
+          Favorita
+          <input
+            type="checkbox"
+            id={ trackId }
+            name="favorite"
+            onChange={ this.handleCheck }
+            data-testid={ `checkbox-music-${trackId}` }
+            checked={ isFav }
+          />
+        </label>
       </div>
     );
   }
 }
 
 MusicCard.propTypes = {
-  songList: propTypes.arrayOf(propTypes.object).isRequired,
+  trackName: propTypes.string.isRequired,
+  previewUrl: propTypes.string.isRequired,
+  trackId: propTypes.number.isRequired,
+  favorites: propTypes.arrayOf(propTypes.object).isRequired,
 };
 
 export default MusicCard;
--- src/pages/Album.js
+++ src/pages/Album.js
@@ -3,6 +3,8 @@ import propTypes from 'prop-types';
 import Header from '../components/Header';
 import getMusics from '../services/musicsAPI';
 import MusicCard from '../components/MusicCard';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from '../components/Loading';
 
 class Album extends React.Component {
   constructor() {
@@ -10,6 +12,8 @@ class Album extends React.Component {
     this.state = {
       album: {},
       songList: [],
+      favorites: [],
+      loading: false,
     };
   }
 
@@ -21,22 +25,37 @@ class Album extends React.Component {
 
   fetchMusic = async () => {
     const { match } = this.props;
+    this.setState({ loading: true });
     const music = await getMusics(match.params.id);
+    const favs = await getFavoriteSongs();
     this.setState({
       album: music[0],
-      songList: music,
+      songList: music.filter((type) => type.kind === 'song'),
+      favorites: favs,
+      loading: false,
     });
   }
 
   render() {
-    const { album, songList } = this.state;
+    const { album, songList, favorites, loading } = this.state;
+    if (loading) return <Loading />;
+    console.log(songList);
     return (
       <div data-testid="page-album">
         <Header />
         <h4 data-testid="album-name">{ album.collectionName }</h4>
         <h5 data-testid="artist-name">{ album.artistName }</h5>
 
-        <MusicCard songList={ songList } />
+        {
+          songList.map((a) => (<MusicCard
+            key={ a.trackId }
+            songList={ songList }
+            trackName={ a.trackName }
+            previewUrl={ a.previewUrl }
+            trackId={ a.trackId }
+            favorites={ favorites }
+          />))
+        }
       </div>
     );
   }
